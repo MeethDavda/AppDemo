f5e78f7ee629b8e613effd2ba15ecf32d09b578a
Meeth <mjdavda02@gmail.com>
2022-11-08 14:11:07 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import Login from "./src/Pages/Login";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="items-center ">
+      <View className=" mt-14 flex flex-col w-[90%] py-3 top-[60%] justify-between bg-slate-200 rounded-md">
+        <Text className="mx-5 text-xl">Enter Username</Text>
+        <TextInput className="p-2 my-2 border-2 border-green-600 w-[85%] ml-5 rounded-md">
+          Name
+        </TextInput>
+
+        <Text className="mx-5 text-xl">Enter Email</Text>
+        <TextInput className="p-2 my-2 border-2 border-green-600 w-[85%] ml-5 rounded-md">
+          Email
+        </TextInput>
+
+        <Button title="Submit" />
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
---
+++ src/Pages/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+function Login() {
+  return (
+    <View>
+      <View>
+        <Text classname="bg-slate-700">hello work</Text>
+      </View>
+    </View>
+  );
+}
+
+export default Login;
